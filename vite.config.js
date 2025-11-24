7eb5817732ca186b62e9d7c65a7d6283a81996e6
goloka-gid <fedorovtcev@gmail.com>
2025-11-24 20:34:26 +0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // base: './' ensures assets are linked relatively, making it easier to deploy 
-  // to GitHub Pages subdirectories without knowing the exact repo name beforehand.
   base: './', 
 })
